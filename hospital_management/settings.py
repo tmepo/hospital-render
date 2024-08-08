29fe6e54ac5d4c0c620c1df62a64474f61915dd9
rederdep <renderdeploy0@gmail.com>
2024-08-09 02:18:14 +0545

    final

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -135,6 +135,4 @@ LOGIN_URL = '/login/'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
