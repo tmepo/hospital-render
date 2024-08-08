7e0df4cfdf117ef8a2a5080a2feeb6ba2f3aa53a
rederdep <renderdeploy0@gmail.com>
2024-08-09 01:21:47 +0545

    satic added

--- hospital_management/settings.py
+++ hospital_management/settings.py
@@ -21,10 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-_69@7p-^9_m%#)8za^it9m0ck)#3a7rfv$qe*azfwt$uig-1d3'
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 DEBUG = os.environ.get("DEBUG","False").lower()=="true"
+# ALLOWED_HOSTS = []
 
 ALLOWED_HOSTS =os.environ.get("ALLOWED_HOSTS").split(" ")
 
